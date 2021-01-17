3c9b4acdde6c42a960bc4b134db83da46219eaa1
Danny McGee <dannymcgee@gmail.com>
2021-01-17 05:11:40 -0500

    Module5

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,13 +1,19 @@
 package module5
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
-/*
+
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
 		fmt.Println("something went wrong")
+
+		return
 	}
 
 	defer resp.Body.Close()
 }
-*/
